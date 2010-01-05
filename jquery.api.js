3ad04afcf3ad10d3556bfe7bfc147a71023c7ffd
Caroline Schnapp <mllegeorgesand@gmail.com>
2010-01-05 03:08:13 -0500

    Added Shopify.updateCartAttributes and Shopify.updateCartNote

--- jquery.api.js
+++ jquery.api.js
@@ -1,12 +1,22 @@
 // (c) Copyright 2009 Jaded Pixel. Author: Caroline Schnapp. All Rights Reserved.
 
+/*
+
+IMPORTANT:
+
+Ajax requests that update Shopify's cart must be queued and sent synchronously to the server.
+Meaning: you must wait for your 1st ajax callback to send your 2nd request, and then wait
+for its callback to send your 3rd request, etc.
+
+*/
+
 if ((typeof Shopify) === 'undefined') {
   Shopify = {};
 }
 
 /* 
 
-Override so that Shopify.formatMoney returns pretty 
+Override so that Shopify.formatMoney returns pretty
 money values instead of cents.
 
 */
@@ -23,11 +33,13 @@ Example override:
   Shopify.onItemAdded = function(line_item) {
     $('message').update('Added '+line_item.title + '...');
   }
+  
 */
 
 Shopify.onError = function(XMLHttpRequest, textStatus) {
   // Shopify returns a description of the error in XMLHttpRequest.responseText.
   // It is JSON.
+  // Example: {"description":"The product 'Amelia - Small' is already sold out.","status":500,"message":"Cart Error"}
   var data = eval('(' + XMLHttpRequest.responseText + ')');
   alert(data.message + '(' + data.status  + '): ' + data.description);
 };
@@ -74,14 +86,17 @@ Shopify.resizeImage = function(image, size) {
   } catch (e) { return image; }
 };
 
-/* API */
-  
+/* Ajax API */
+
+// -------------------------------------------------------------------------------------
+// POST to cart/add.js returns the JSON of the line item associated with the added item.
+// -------------------------------------------------------------------------------------
 Shopify.addItem = function(variant_id, quantity, callback) {
   var quantity = quantity || 1;
   var params = {
     type: 'POST',
     url: '/cart/add.js',
-    data: 'quantity='+quantity+'&id='+variant_id,
+    data: 'quantity=' + quantity + '&id=' + variant_id,
     dataType: 'json',
     success: function(line_item) { 
       if ((typeof callback) === 'function') {
@@ -93,32 +108,38 @@ Shopify.addItem = function(variant_id, quantity, callback) {
     },
     error: function(XMLHttpRequest, textStatus) {
       Shopify.onError(XMLHttpRequest, textStatus);
-    }   
+    }
   };
   jQuery.ajax(params);
 };
 
+// ---------------------------------------------------------
+// POST to cart/add.js returns the JSON of the line item.
+// ---------------------------------------------------------
 Shopify.addItemFromForm = function(form_id, callback) {
     var params = {
       type: 'POST',
       url: '/cart/add.js',
       data: jQuery('#' + form_id).serialize(),
       dataType: 'json',
-      success: function(cart) { 
+      success: function(line_item) { 
         if ((typeof callback) === 'function') {
-          callback(cart);
+          callback(line_item);
         }
         else {
-          Shopify.onItemAdded(cart);
+          Shopify.onItemAdded(line_item);
         }
       },
       error: function(XMLHttpRequest, textStatus) {
         Shopify.onError(XMLHttpRequest, textStatus);
-      }   
+      }
     };
     jQuery.ajax(params);
 };
 
+// ---------------------------------------------------------
+// GET cart.js returns the cart in JSON.
+// ---------------------------------------------------------
 Shopify.getCart = function(callback) {
   jQuery.getJSON('/cart.js', function (cart, textStatus) {
     if ((typeof callback) === 'function') {
@@ -126,21 +147,27 @@ Shopify.getCart = function(callback) {
     }
     else {
       Shopify.onCartUpdate(cart);
-    }   
-  }); 
-};  
+    }
+  });
+};
 
+// ---------------------------------------------------------
+// GET products/<product-handle>.js returns the product in JSON.
+// ---------------------------------------------------------
 Shopify.getProduct = function(handle, callback) {
-  jQuery.getJSON('/products/'+handle+'.js', function (product, textStatus) {
+  jQuery.getJSON('/products/' + handle + '.js', function (product, textStatus) {
     if ((typeof callback) === 'function') {
       callback(product);
     }
     else {
       Shopify.onProduct(product);
-    } 
+    }
   });
 };
 
+// ---------------------------------------------------------
+// POST to cart/change.js returns the cart in JSON.
+// ---------------------------------------------------------
 Shopify.changeItem = function(variant_id, quantity, callback) {
   var params = {
     type: 'POST',
@@ -159,9 +186,12 @@ Shopify.changeItem = function(variant_id, quantity, callback) {
       Shopify.onError(XMLHttpRequest, textStatus);
     }
   };
-  jQuery.ajax(params);            
+  jQuery.ajax(params);
 };
 
+// ---------------------------------------------------------
+// POST to cart/change.js returns the cart in JSON.
+// ---------------------------------------------------------
 Shopify.removeItem = function(variant_id, callback) {
   var params = {
     type: 'POST',
@@ -183,6 +213,11 @@ Shopify.removeItem = function(variant_id, callback) {
   jQuery.ajax(params);
 };
 
+// ---------------------------------------------------------
+// POST to cart/clear.js returns the cart in JSON.
+// It removes all the items in the cart, but does
+// not clear the cart attributes nor the cart note.
+// ---------------------------------------------------------
 Shopify.clear = function(callback) {
   var params = {
     type: 'POST',
@@ -204,11 +239,82 @@ Shopify.clear = function(callback) {
   jQuery.ajax(params);
 };
 
+// ---------------------------------------------------------
+// POST to cart/update.js returns the cart in JSON.
+// ---------------------------------------------------------
 Shopify.updateCartFromForm = function(form_id, callback) {
   var params = {
     type: 'POST',
     url: '/cart/update.js',
     data: jQuery('#' + form_id).serialize(),
+    dataType: 'json',
+    success: function(cart) {
+      if ((typeof callback) === 'function') {
+        callback(cart);
+      }
+      else {
+        Shopify.onCartUpdate(cart);
+      }
+    },
+    error: function(XMLHttpRequest, textStatus) {
+      Shopify.onError(XMLHttpRequest, textStatus);
+    }
+  };
+  jQuery.ajax(params);
+};
+
+// ---------------------------------------------------------
+// POST to cart/update.js returns the cart in JSON.
+// To clear a particular attribute, set its value to an empty string.
+// Receives attributes as a hash or array. Look at comments below.
+// ---------------------------------------------------------
+Shopify.updateCartAttributes = function(attributes, callback) {
+  var data = '';
+  // If attributes is an array of the form:
+  // [ { key: 'my key', value: 'my value' }, ... ]
+  if (jQuery.isArray(attributes)) {
+    jQuery.each(attributes, function(indexInArray, valueOfElement) {
+      var key = attributeToString(valueOfElement.key);
+      if (key !== '') {
+        data += 'attributes[' + key + ']=' + attributeToString(valueOfElement.value) + '&';
+      }
+    });
+  }
+  // If attributes is a hash of the form:
+  // { 'my key' : 'my value', ... }
+  else if ((typeof attributes === 'object') && attributes !== null) {
+    jQuery.each(attributes, function(key, value) {
+        data += 'attributes[' + attributeToString(key) + ']=' + attributeToString(value) + '&';
+    });
+  }
+  var params = {
+    type: 'POST',
+    url: '/cart/update.js',
+    data: data,
+    dataType: 'json',
+    success: function(cart) {
+      if ((typeof callback) === 'function') {
+        callback(cart);
+      }
+      else {
+        Shopify.onCartUpdate(cart);
+      }
+    },
+    error: function(XMLHttpRequest, textStatus) {
+      Shopify.onError(XMLHttpRequest, textStatus);
+    }
+  };
+  jQuery.ajax(params);
+};
+
+// ---------------------------------------------------------
+// POST to cart/update.js returns the cart in JSON.
+// ---------------------------------------------------------
+Shopify.updateCartNote = function(note, callback) {
+  var params = {
+    type: 'POST',
+    url: '/cart/update.js',
+    data: 'note=' + attributeToString(note),
     dataType: 'json',
     success: function(cart) {
       if ((typeof callback) === 'function') {
@@ -231,4 +337,18 @@ function floatToString(numeric, decimals) {
   var amount = numeric.toFixed(decimals).toString();
   if(amount.match(/^\.\d+/)) {return "0"+amount; }
   else { return amount; }
+}
+
+/* Used by API */
+
+function attributeToString(attribute) {
+  if ((typeof attribute) !== 'string') {
+    // Converts to a string.
+    attribute += '';
+    if (attribute === 'undefined') {
+      attribute = '';
+    }
+  }
+  // Removing leading and trailing whitespace.
+  return jQuery.trim(attribute);
 }
